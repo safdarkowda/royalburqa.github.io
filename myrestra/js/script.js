6aa7b8e72b6b72e6d8962187aa64adb948d79ac8
safdarkowda <38979109+safdarkowda@users.noreply.github.com>
2018-08-07 21:48:26 +0530

    change

---
+++ myrestra/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$("#header, .info").ripples({
+  dropRadius: 25,
+  perturbance: 0.6,
+  
+});
+	//magnific popup
+	$('.items-container').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  
+  gallery:{
+	  enabled:true
+  }
+  
+  
+  
+  
+  // other options
+  
+  
+  
+});
+	
+	
+	
+	
+	
+});
